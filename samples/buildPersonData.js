2f062ff8d702670216a5d4a3d10799554aa5631a
Wahid Rahim <wrahim@efemme.com>
2016-11-24 17:00:19 -0500

    gitignore

--- samples/buildPersonData.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function buildPersonData(_a) {
-    var fname = _a.fname, lname = _a.lname, addr = _a.addr;
-    return fname + " " + lname + " " + addr;
-}
-exports.buildPersonData = buildPersonData;
-exports.DEMO = 'demo';
-var message = "a simple message";
-message = 0;
--- server/queries/findAllCourses.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var model_1 = require("../model/model");
-function findAllCourses() {
-    return model_1.CourseModel.findAll({
-        order: ['seqNo']
-    });
-}
-exports.findAllCourses = findAllCourses;
